cc2570aaf63cd567e1cf697cb90c2f0d57bb076f
Alok Patel <alok0590@gmail.com>
2021-03-01 16:31:44 +0530

    #4689: Fixed PHPCS magento standard errors.

--- Services/Store/StoreStockConfig.php
+++ Services/Store/StoreStockConfig.php
@@ -4,23 +4,23 @@ namespace Wizzy\Search\Services\Store;
 
 class StoreStockConfig
 {
-   private $configManager;
+    private $configManager;
 
-   const SHOW_OUT_OF_STOCK_PRODUCTS = "cataloginventory/options/show_out_of_stock";
-   private $storeId;
+    const SHOW_OUT_OF_STOCK_PRODUCTS = "cataloginventory/options/show_out_of_stock";
+    private $storeId;
 
-   public function __construct(ConfigManager $configManager)
-   {
-      $this->configManager = $configManager;
-   }
+    public function __construct(ConfigManager $configManager)
+    {
+        $this->configManager = $configManager;
+    }
 
-   public function setStore(string $storeId)
-   {
-      $this->storeId = $storeId;
-   }
+    public function setStore(string $storeId)
+    {
+        $this->storeId = $storeId;
+    }
 
-   public function hasToIncludeOutOfStockProducts()
-   {
-      return ($this->configManager->getStoreConfig(self::SHOW_OUT_OF_STOCK_PRODUCTS, $this->storeId) == 1);
-   }
+    public function hasToIncludeOutOfStockProducts()
+    {
+        return ($this->configManager->getStoreConfig(self::SHOW_OUT_OF_STOCK_PRODUCTS, $this->storeId) == 1);
+    }
 }
