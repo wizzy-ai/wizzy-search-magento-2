0e39d500c08ba48b23d53f3c8fb1d88dfedc9626
Smit Kandiya <smit@wizzy.ai>
2022-05-03 15:39:31 +0530

    WZ-144 : Add fields in Admin Panel to configure Infinite scroll offset size

--- Services/Setup/Version1317.php
+++ Services/Setup/Version1317.php
@@ -18,6 +18,10 @@ class Version1317
         "wizzy_autocomplete_configuration/autocomplete_pages/sync_pages" => 0,
         "wizzy_catalogue_configuration/catalogue_configuration_images/replace_child_with_main_image" => 0,
         "wizzy_catalogue_configuration/catalogue_configuration_name/replace_child_name_with_parent" => 1,
+        "wizzy_search_configuration/search_results_pagination_configuration/infinite_scroll_offset_desktop" =>
+        600,
+        "wizzy_search_configuration/search_results_pagination_configuration/infinite_scroll_offset_mobile" =>
+        700,
     ];
 
     public function update(SchemaSetupInterface $setup)
