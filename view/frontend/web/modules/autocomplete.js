9945a780600f07bbc1694ca5ae2da324ac0220fe
Smit Kandiya <smit@wizzy.ai>
2022-05-19 10:27:06 +0530

    WZ-210: Fixed lib to pick search input from config

--- view/frontend/web/modules/autocomplete.js
+++ view/frontend/web/modules/autocomplete.js
@@ -1,6 +1,6 @@
-requirejs(['jquery', 'wizzy/libs/autocomplete'], function($, wA) {
+requirejs(['jquery', 'wizzy/libs/autocomplete', 'wizzy/utils/search'], function($, wA, searchUtils) {
     $(document).ready(function(e) {
-        $('.wizzy-search-input').each(function(e) {
+        $(searchUtils.getInputDOM()).each(function(e) {
             var config = window.wizzyConfig.autocomplete.menu.view;
             config['element'] = $(this);
             config ['formSubmissionBehaviour'] = window.wizzyConfig.search.configs.general.formSubmissionBehaviour;
--- view/frontend/web/search/form.mini.js
+++ view/frontend/web/search/form.mini.js
@@ -1,9 +1,9 @@
-requirejs(['jquery', 'wizzy/libs/searchUrlUtils'], function($, urlUtils) {
+requirejs(['jquery', 'wizzy/libs/searchUrlUtils', 'wizzy/utils/search'], function($, urlUtils, searchUtils) {
     function hasToEnableFullScreen() {
         return (window.wizzyConfig.autocomplete.enabled || window.wizzyConfig.search.configs.general.behaviour === "search_as_you_type");
     }
 
-    $('body').on('click', '.wizzy-search-glass, .wizzy-search-input', function(e) {
+    $('body').on('click', '.wizzy-search-glass, '+ searchUtils.getInputDOM(), function(e) {
         e.preventDefault();
         if (hasToEnableFullScreen()) {
             var windowWidth = $(window).width();
@@ -11,7 +11,7 @@ requirejs(['jquery', 'wizzy/libs/searchUrlUtils'], function($, urlUtils) {
                 $(this).parents('.wizzy-search-form-wrapper').addClass('mobileTapped');
                 $('.wizzy-search-empty-results-wrapper').addClass('mobileTapped');
             }
-            $(this).parents('.wizzy-search-form-wrapper').find('.wizzy-search-input').focus();
+            $(this).parents('.wizzy-search-form-wrapper').find(searchUtils.getInputDOM()).focus();
             $('.wizzy-search-wrapper').addClass('mobileTapped');
         }
     });
@@ -24,7 +24,7 @@ requirejs(['jquery', 'wizzy/libs/searchUrlUtils'], function($, urlUtils) {
 
     $('body').on('click', '.wizzy-search-back', function(e) {
         e.preventDefault();
-        var searchInput = $(this).parents('.wizzy-search-form-wrapper').find('.wizzy-search-input');
+        var searchInput = $(this).parents('.wizzy-search-form-wrapper').find(searchUtils.getInputDOM());
         $(this).parents('.wizzy-search-form-wrapper').removeClass('mobileTapped');
         $('.wizzy-search-wrapper').removeClass('mobileTapped');
         $('.wizzy-search-empty-results-wrapper').removeClass('mobileTapped');
@@ -37,13 +37,13 @@ requirejs(['jquery', 'wizzy/libs/searchUrlUtils'], function($, urlUtils) {
 
     $('body').on('click', '.wizzy-search-clear', function(e) {
         e.preventDefault();
-        var searchInput = $(this).parents('.wizzy-search-form-wrapper').find('.wizzy-search-input');
+        var searchInput = $(this).parents('.wizzy-search-form-wrapper').find(searchUtils.getInputDOM());
         searchInput.val('');
         searchInput.trigger('keyup');
         searchInput.focus();
     });
 
-    $('body').on('keyup', '.wizzy-search-input',function (e) {
+    $('body').on('keyup', searchUtils.getInputDOM(),function (e) {
         var value = $(this).val().trim();
         if (value == "") {
             $(this).parents('.wizzy-search-form-wrapper').find('.wizzy-search-clear').fadeOut(100);
