5e005ec7f1d6e86de2298e2d12e5bc2fef937600
Alok Patel <alok0590@gmail.com>
2022-07-04 15:34:49 +0530

    WZ-143: Limiting the number of pages to be displayed on search page

--- view/frontend/web/renderers/components/search/pagination.js
+++ view/frontend/web/renderers/components/search/pagination.js
@@ -7,6 +7,7 @@ define(['jquery', 'Mustache', 'wizzy/libs/pageStore', 'wizzy/utils/pagination'],
 
         var totalPages = paginationUtils.getTotalPages();
         var currentPage = paginationUtils.getCurrentPage();
+        var maxConnectedPages = 3;
 
         if (response === null || totalPages <= 1) {
             return '';
@@ -14,23 +15,52 @@ define(['jquery', 'Mustache', 'wizzy/libs/pageStore', 'wizzy/utils/pagination'],
 
         var pageItems = [];
         var pageIndex = 1;
-        while(pageIndex <= totalPages) {
-            pageItems.push({
-               value: pageIndex,
-               isSelected: (pageIndex == currentPage)
-            });
-            pageIndex++;
+        if(currentPage > 1) {
+            pageItems = pushPageItems(pageItems, '...', false, 'inactive eclipse');
         }
-        
+        if(totalPages <= 3) {
+            while(pageIndex <= totalPages) {
+                pageItems = pushPageItems(pageItems, pageIndex, (pageIndex == currentPage));
+                pageIndex++;
+            }
+        } else {
+            let count = totalPages - currentPage > 3 ? 0 : totalPages - currentPage;
+            if(totalPages - currentPage > 3) {
+                while(count < maxConnectedPages) {
+                    pageItems = pushPageItems(pageItems, currentPage + count, (currentPage + count == currentPage));
+                    count++;
+                }
+                if(currentPage <= totalPages - 3) {
+                    pageItems = pushPageItems(pageItems, '...', false, 'inactive eclipse')
+                }
+                pageItems = pushPageItems(pageItems, totalPages, (totalPages == currentPage));
+            } else {
+                let page = currentPage
+                while(page <= totalPages) {
+                    pageItems = pushPageItems(pageItems, page, (page == currentPage));
+                    page++;
+                }
+            }
+        }
+
         var templates = window.wizzyConfig.search.view.templates;
-        var paginationTemplate = $(templates.pagination).html();
+        var paginationTemplate = $wZ(templates.pagination).html();
         paginationTemplate = Mustache.render(paginationTemplate, {
             items: pageItems,
             isPrevActive: (currentPage > 1),
             isNextActive: (currentPage < totalPages),
         });
-        
+
         return paginationTemplate;
+    }
+
+    function pushPageItems(pageItems, value, isSelected, customClass = "") {
+        pageItems.push({
+            value: value,
+            isSelected: isSelected,
+            customClass:customClass
+        })
+        return pageItems;
     }
 
     return {
