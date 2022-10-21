b95ca1a816d7ec09d2e521a6482a143fc45d4347
Smit Kandiya <smit@wizzy.ai>
2022-10-21 08:53:36 +0530

    WZ-147:-Added a button to copy the configuration from Another Store

---
+++ Services/Store/StoreCopyConfig.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Wizzy\Search\Services\Store;
+
+use Magento\Framework\App\Request\Http;
+use Wizzy\Search\Services\Store\ConfigManager;
+
+class StoreCopyConfig
+{
+    const WIZZY_COPY_CONFIGURATION = "wizzy_copy_configuration";
+    const COPY_CONFIGURATION = self::WIZZY_COPY_CONFIGURATION . "/copy_configuration";
+    const COPY_CONFIGURATION_FROM = self::COPY_CONFIGURATION . "/from_store";
+
+    public function __construct(
+        Http $request,
+        ConfigManager $configManager
+    ) {
+        $this->request = $request;
+        $this->configManager = $configManager;
+        $this->storeId = $this->request->getParam('store');
+    }
+
+    public function getCopyConfigFrom()
+    {
+        $copyConfigFrom = $this->configManager->getStoreConfig(self::COPY_CONFIGURATION_FROM, $this->storeId);
+        return $copyConfigFrom;
+    }
+}
