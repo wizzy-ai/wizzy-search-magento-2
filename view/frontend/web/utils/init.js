1dadea3947487f5e89389e69fb285d3ea911dbc2
Smit Kandiya <smit@wizzy.ai>
2022-12-23 14:30:00 +0530

    WZ-858: Fixed-Issue with the Bestseller Collection on error page.

--- view/frontend/web/utils/init.js
+++ view/frontend/web/utils/init.js
@@ -1,12 +1,15 @@
 requirejs(['wizzy/renderers/dom', 'wizzy/renderers/search', 'wizzy/renderers/noProductsFound', 'wizzy/fetchers/filters'], function(domRenderer, searchRenderer, noProductsFoundRenderer, filtersFetcher) {
     wizzy.registerEvent(wizzy.allowedEvents.EMPTY_RESULTS_RENDERED, function(payload) {
-        if (window.wizzyConfig.search.configs != undefined && window.wizzyConfig.search.configs.noProductsFound) {
+
+        if (typeof window.wizzyConfig.search.configs != 'undefined' && window.wizzyConfig.search.configs.noProductsFound) {
             var noProductsFoundDefaultBehaviour = window.wizzyConfig.search.configs.noProductsFound
         }
-        if (window.wizzyConfig.pageStore.groupedFilteredProducts != undefined && window.wizzyConfig.pageStore.groupedFilteredProducts.noProductsFound) {
+
+        if (typeof window.wizzyConfig.pageStore.groupedFilteredProducts != 'undefined' && window.wizzyConfig.pageStore.groupedFilteredProducts.noProductsFound) {
             var defaultData = window.wizzyConfig.pageStore.groupedFilteredProducts.noProductsFound;
         }
-            if (defaultData != undefined && noProductsFoundDefaultBehaviour != undefined  &&noProductsFoundDefaultBehaviour.showProducts) {
+            
+        if (typeof noProductsFoundDefaultBehaviour != 'undefined'  && noProductsFoundDefaultBehaviour.showProducts) {
                 searchRenderer.showLoaderForSpecificDOM(domRenderer.getNoProductsFoundDOM());
                 if(!defaultData) {
                     var defaultPool = noProductsFoundDefaultBehaviour.defaultPool;
@@ -19,8 +22,8 @@ requirejs(['wizzy/renderers/dom', 'wizzy/renderers/search', 'wizzy/renderers/noP
                 } else {
                     noProductsFoundRenderer.showProductsWithFailedSearch();
                 }                
-            }
-            return payload;
+        }
+        return payload;
     });
 
 });
