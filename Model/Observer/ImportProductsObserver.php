34912fc461904cf629108a4dca7d8776e2836f2a
smit-kandiya <smit@wizzy.ai>
2023-04-28 15:05:16 +0530

    WZ-1972: M2:Updating child product using CSV is not adding it's parent product id for sync.

--- Model/Observer/ImportProductsObserver.php
+++ Model/Observer/ImportProductsObserver.php
@@ -4,6 +4,8 @@ namespace Wizzy\Search\Model\Observer;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Wizzy\Search\Services\Catalogue\ProductsManager;
+use Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable;
+
 use Wizzy\Search\Services\Indexer\IndexerManager;
 
 class ImportProductsObserver implements ObserverInterface
@@ -11,10 +13,12 @@ class ImportProductsObserver implements ObserverInterface
     private $indexer;
     public function __construct(
         IndexerManager $indexerManager,
-        ProductsManager $productsManager
+        ProductsManager $productsManager,
+        Configurable $configurable
     ) {
         $this->indexer = $indexerManager->getProductsIndexer();
         $this->productsManager = $productsManager;
+        $this->configurable = $configurable;
     }
     public function execute(Observer $observer)
     {
@@ -22,8 +26,12 @@ class ImportProductsObserver implements ObserverInterface
         $SKUs = array_column($bunch, 'sku');
         $products = $this->productsManager->getProductsBySKUs($SKUs);
         $productIds = $this->productsManager->getProductIds($products);
-        if (count($productIds) > 0) {
-            $this->indexer->reindexList($productIds);
+        $parentProductIds = $this->configurable->getParentIdsByChild($productIds);
+
+        $productIdsToBeSynced = array_unique(array_merge($productIds, $parentProductIds));
+
+        if (count($productIdsToBeSynced) > 0) {
+            $this->indexer->reindexList($productIdsToBeSynced);
         }
     }
 }
