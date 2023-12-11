55704c4a995996552c260e5908fa8ee404b5e2b0
Smit Kandiya <smit@wizzy.ai>
2023-12-11 18:38:23 +0530

    WZ-3164: Implemented getDeletedProducts in new indexer.

--- Controller/Adminhtml/Queue/Clear.php
+++ Controller/Adminhtml/Queue/Clear.php
@@ -7,11 +7,13 @@ use Magento\Framework\Controller\ResultFactory;
 use Magento\Backend\App\Action;
 use Wizzy\Search\Helpers\FlashMessagesManager;
 use Wizzy\Search\Services\Queue\QueueManager;
+use Wizzy\Search\Model\Indexer\RecoverDeletedProducts;
 
 class Clear extends Action
 {
     private $queueManager;
     private $flashMessagesManager;
+    private $recoverDeletedProducts;
 
    /**
     * @param Context $context
@@ -21,11 +23,13 @@ class Clear extends Action
     public function __construct(
         Context $context,
         QueueManager $queueManager,
-        FlashMessagesManager $flashMessagesManager
+        FlashMessagesManager $flashMessagesManager,
+        RecoverDeletedProducts $recoverDeletedProducts
     ) {
         parent::__construct($context);
         $this->queueManager = $queueManager;
         $this->flashMessagesManager = $flashMessagesManager;
+        $this->recoverDeletedProducts = $recoverDeletedProducts;
     }
 
     public function execute()
@@ -42,7 +46,7 @@ class Clear extends Action
         } else {
             $this->flashMessagesManager->warning('No processor in queue at the moment.');
         }
-
+        $this->recoverDeletedProducts->executeFull();
         return $resultRedirect;
     }
 }
