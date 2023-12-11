d06f102b6707adffb1455e5731a01c7f5ae68ab2
Smit Kandiya <smit@wizzy.ai>
2023-12-11 18:25:26 +0530

    WZ-3164: Implemented getDeletedProducts in new indexer.

--- Controller/Adminhtml/Queue/Truncate.php
+++ Controller/Adminhtml/Queue/Truncate.php
@@ -7,11 +7,13 @@ use Magento\Framework\Controller\ResultFactory;
 use Magento\Backend\App\Action;
 use Wizzy\Search\Helpers\FlashMessagesManager;
 use Wizzy\Search\Services\Queue\QueueManager;
+use Wizzy\Search\Model\Indexer\RecoverDeletedProducts;
 
 class Truncate extends Action
 {
     private $queueManager;
     private $flashMessagesManager;
+    private $recoverDeletedProducts;
 
    /**
     * @param Context $context
@@ -21,11 +23,13 @@ class Truncate extends Action
     public function __construct(
         Context $context,
         QueueManager $queueManager,
-        FlashMessagesManager $flashMessagesManager
+        FlashMessagesManager $flashMessagesManager,
+        RecoverDeletedProducts $recoverDeletedProducts
     ) {
         parent::__construct($context);
         $this->queueManager = $queueManager;
         $this->flashMessagesManager = $flashMessagesManager;
+        $this->recoverDeletedProducts = $recoverDeletedProducts;
     }
 
     public function execute()
@@ -44,7 +48,7 @@ class Truncate extends Action
                 'No completed/cancelled processor to truncate in queue at the moment.'
             );
         }
-
+        $this->recoverDeletedProducts->executeFull();
         return $resultRedirect;
     }
 }
