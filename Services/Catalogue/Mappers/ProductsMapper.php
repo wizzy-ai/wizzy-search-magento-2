a59b5007532a795d6dd8c3ffabc1d07d5f5593a0
smit-kandiya <smit@wizzy.ai>
2024-10-03 17:25:28 +0530

    WZ-4569: Added category ID in Wizzy's category object.

--- Services/Catalogue/Mappers/ProductsMapper.php
+++ Services/Catalogue/Mappers/ProductsMapper.php
@@ -863,6 +863,7 @@ class ProductsMapper
          'id'  =>
             (!empty($category['urlKey']) && $category['urlKey'] != null)
                ? $category['urlKey'] : $category['id'],
+         'machineId' => $category['id'],
          'url' => $category['url'],
          'position' => (int) $category['position'],
          'level'  => (int) $category['level'],
