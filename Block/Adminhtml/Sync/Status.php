c525d98d94ad44728aa9637831d719ab7f3fce7a
smit-kandiya <smit@wizzy.ai>
2024-10-24 15:52:19 +0530

    WZ-4212: Added more details in Wizzy section on Product Details Page - When a Product is Skipped

--- Block/Adminhtml/Sync/Status.php
+++ Block/Adminhtml/Sync/Status.php
@@ -8,22 +8,26 @@ use Wizzy\Search\Services\Model\EntitiesSync;
 use Magento\Store\Model\StoreManagerInterface;
 use Wizzy\Search\Services\Store\StoreManager;
 use Magento\Framework\View\Element\Template;
+use Wizzy\Search\Services\Model\SyncSkippedEntities;
 
 class Status extends Template
 {
     protected $_template = 'sync/status.phtml';
+    public $syncSkippedEntities;
     public function __construct(
         EntitiesSync $entitiesSync,
         Context $context,
         Registry $registry,
         StoreManagerInterface $storeManager,
         StoreManager $_storemanager,
+        SyncSkippedEntities $syncSkippedEntities,
         array $data = []
     ) {
         $this->entitiesSync = $entitiesSync;
         $this->registry = $registry;
         $this->storeManager = $storeManager;
         $this->_storemanager = $_storemanager;
+        $this->syncSkippedEntities = $syncSkippedEntities;
         parent::__construct($context, $data);
     }
 
@@ -32,22 +36,31 @@ class Status extends Template
         $entityId = $this->registry->registry('current_product')->getId();
         $storeId = $this->storeManager->getStore()->getId();
         $storeIds = $this->_storemanager->getToSyncStoreIds($storeId);
-        $syncResults = [];
-
+        $entitiesSyncStatus_ = [];
         foreach ($storeIds as $storeId) {
-            $entityStatus = $this->entitiesSync->getEntitiesSyncStatus($entityId, $storeId, 'product');
-            if (count($entityStatus) == 0) {
-                $entityStatus = [
-                    [
+            $entitiesSyncStatus = $this->entitiesSync->getEntitiesSyncStatus($entityId, $storeId, 'product');
+            $skippedEntitiesData = $this->syncSkippedEntities->getSkippedEntityById($storeId, $entityId, 'product');
+
+            if ($entitiesSyncStatus) {
+                foreach ($entitiesSyncStatus as $entitySyncStatus) {
+                    $entitiesSyncStatus_[] = [
+                        "store_id" => $entitySyncStatus['store_id'],
+                        "status" => $entitySyncStatus['status'],
+                        "updated_at" => $entitySyncStatus['updated_at'],
+                        "skipped_data" => ($skippedEntitiesData && !empty($skippedEntitiesData))
+                        ? $skippedEntitiesData
+                        : null,
+                    ];
+                }
+            } else {
+                $entitiesSyncStatus_[] = [
                     "store_id" => $storeId,
                     "status" => -1,
-                    "updated_at" => "&nbsp;-"
-                    ]
+                    "updated_at" => "-"
                 ];
             }
-            $syncResults[] = $entityStatus;
-        }
 
-        return $syncResults;
+        }
+        return $entitiesSyncStatus_;
     }
 }
