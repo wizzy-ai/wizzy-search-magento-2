2fcc5bd9abf83f25b4c940b9ea1351ee1821e774
smit-kandiya <smit@wizzy.ai>
2024-12-30 19:34:18 +0530

    WZ-5199_: Removed the changes done for category URL fix while multiple stores

--- Services/Catalogue/Mappers/ProductsMapper.php
+++ Services/Catalogue/Mappers/ProductsMapper.php
@@ -112,7 +112,6 @@ class ProductsMapper
         $this->storeCatalogueConfig->setStore($storeId);
         $this->productURLManager->setStore($storeId);
         $this->productURLManager->fetchUrls($products);
-        $this->configurableProductsData->setStore($storeId);
         $this->setAdminUrl();
         $this->isBrandMandatory = $this->storeCatalogueConfig->isBrandMandatoryForSync();
         $this->resetEntitiesToIgnore();
